7f85fb9cd858afa1fe37750740df0b498fa8618a
Fluffel₿uff ⚡️ <113269946+fluffelpuff@users.noreply.github.com>
2025-02-28 23:18:41 +0100

    Update __init__.py

--- custom_components/fluffelsha/__init__.py
+++ custom_components/fluffelsha/__init__.py
@@ -1,8 +1,18 @@
 # config/custom_components/fluffelsha/__init__.py
 
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Fluffelsha component."""
-    # Hier können Sie zusätzliche Initialisierungen vornehmen, falls erforderlich.
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the Fluffelsha integration from a config entry."""
+    # Richten Sie die Button-Entität ein
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "button")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload the Fluffelsha integration."""
+    # Entladen Sie die Button-Entität
+    await hass.config_entries.async_forward_entry_unload(entry, "button")
     return True
