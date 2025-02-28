4303dda30b136bac40a396fc26aad6ae522ad745
Fluffel₿uff ⚡️ <113269946+fluffelpuff@users.noreply.github.com>
2025-02-28 22:56:07 +0100

    updates

--- custom_components/fluffelsha/__init__.py
+++ custom_components/fluffelsha/__init__.py
@@ -1,7 +1,6 @@
 # config/custom_components/fluffelsha/__init__.py
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Fluffelsha component."""
--- custom_components/fluffelsha/button.py
+++ custom_components/fluffelsha/button.py
@@ -1,22 +1,23 @@
 # config/custom_components/fluffelsha/button.py
 
 from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 class FluffelshaButton(ButtonEntity):
     """Button-Entität für FluffelsHA."""
 
     def __init__(self):
         self._attr_name = "FluffelsHA Button"
+        self._attr_unique_id = "fluffelsha_button"
 
     def press(self):
         """Handle the button press."""
+        # Hier können Sie die Logik für den Button-Klick hinzufügen.
         self.hass.services.call("notify", "mobile_app", {
             "message": "Der FluffelsHA Button wurde gedrückt!"
         })
 
 
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the Fluffelsha platform."""
+    """Set up the Fluffelsha button platform."""
     async_add_entities([FluffelshaButton()])
